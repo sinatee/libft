9bc7ddb1159b642dd07e9193b8aa96ea84c99a70
sinatee <supapipatsinatee@gmail.com>
2026-09-09 22:35:40 +0700

    strlcat

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tomato-soup <tomato-soup@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 17:06:51 by sisupapi          #+#    #+#             */
-/*   Updated: 2026/09/09 00:21:28 by tomato-soup      ###   ########.fr       */
+/*   Updated: 2026/09/09 21:01:52 by tomato-soup      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
